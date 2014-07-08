653db94f994594fbecfaeef7048edb12c528f95a
Joachim Strömbergson <joachim@secworks.se>
2014-07-08 18:53:18 +0200

    Merging the node class.

--- src/huffman.py
+++ src/huffman.py
@@ -29,6 +29,37 @@ DEBUG = True
 
 
 #-------------------------------------------------------------------
+# class Node
+#
+# A basic node class (struct) used to create the prefix tree.
+#-------------------------------------------------------------------
+class Node:
+    def __init__(self, char, weight):
+        self.char = char
+        self.weight = weight
+        self.lchild = None
+        self.rchild = None
+        self.children = 0
+
+    def print_fields(self):
+        if self.char != None:
+            print("Char: %c, weight = %d" % (self.char, self.weight))
+        else:
+            print("Inner node with %d children and total weight %d" %\
+                  (self.children, self.weight))
+
+        if self.lchild != None:
+            print("Left child contains subtree")
+        else:
+            print("Left child contains no subtree")
+
+        if self.rchild != None:
+            print("Right child contains subtree")
+        else:
+            print("Right child contains no subtree")
+
+
+#-------------------------------------------------------------------
 #
 # Note: We assume that the file contains bytes.
 #-------------------------------------------------------------------
